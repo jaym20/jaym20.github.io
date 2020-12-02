d83c96eb9f279d18bd906b79e9918a655d30ec24
Jay Makwana <32141072+jaym20@users.noreply.github.com>
2020-12-02 09:34:12 +0530

    script modified

--- assets/js/script-jquery.js
+++ assets/js/script-jquery.js
@@ -23,13 +23,13 @@ window.onload = function () {
   }
 };
 
-// $(document).keydown(function (event) {
-//     if (event.keyCode == 123) { // Prevent F12
-//         return false;
-//     } else if (event.ctrlKey && event.shiftKey && event.keyCode == 73) { // Prevent Ctrl+Shift+I
-//         return false;
-//     }
-// });
+$(document).keydown(function (event) {
+    if (event.keyCode == 123) { // Prevent F12
+        return false;
+    } else if (event.ctrlKey && event.shiftKey && event.keyCode == 73) { // Prevent Ctrl+Shift+I
+        return false;
+    }
+});
 
 $("#mob-icon").click(() => {
   if ($("#mob-icon").html() == "×") {
